ff2df32aab06b8e1ba63654658a107e04cb52bf2
AxDjuraev <axdjuraev@gmail.com>
2023-03-11 12:10:08 +0500

    update (main): feat init client

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import logging
-from ulauncher.api.client.Extension import Extension
+from asyncio import new_event_loop
+
+from telethon.sync import TelegramClient
 from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+from ulauncher.api.shared.action.RenderResultListAction import \
+    RenderResultListAction
 from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
-
+loop = new_event_loop()
 icon_file='images/icon.png'
-
+client = None
 
 class TelegramSeachExtension(Extension):
     def __init__(self):
@@ -19,6 +23,19 @@ class TelegramSeachExtension(Extension):
 
 class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
+        global client
+        if client is None:
+            api_id = extension.preferences['telegram_client_api_id']
+            api_hash = extension.preferences['telegram_client_api_hash']
+            session_path = extension.preferences['telegram_client_session_path']
+            client = TelegramClient(
+                session_path,
+                api_id=api_id,
+                api_hash=api_hash
+            )
+            if not loop.run_until_complete(client.is_user_authorized()):
+                raise ValueError("Client is not authorized!")
+
         query = event.get_argument() or str()
 
         if len(query.strip()) == 0:
